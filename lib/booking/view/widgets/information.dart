50105cf9b07276a1ab8b706877e1647b5bd8e008
Musa <94803483+musakhamidullin@users.noreply.github.com>
2023-09-10 16:21:30 +0300

    add mask

--- lib/booking/view/widgets/information.dart
+++ lib/booking/view/widgets/information.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 import '../../../theme.dart';
 
@@ -9,40 +10,46 @@ class InformationWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return DecoratedBox(
-      decoration: const BoxDecoration(
-        color: Colors.white,
-        borderRadius: borderRadius
-      ),
+      decoration:
+          const BoxDecoration(color: Colors.white, borderRadius: borderRadius),
       child: Padding(
         padding: paddingWidgetVH,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Информация о клиенте', style: theme.textTheme.titleLarge,),
+            Text(
+              'Информация о клиенте',
+              style: theme.textTheme.titleLarge,
+            ),
             padding,
-            const TextField(
-              
-              decoration: InputDecoration(
-                hintText: 'Номер телефона',
-                border: InputBorder.none,
-                floatingLabelBehavior: FloatingLabelBehavior.always,
+            TextFormField(
+              keyboardType: TextInputType.phone,
+              inputFormatters: [
+                MaskTextInputFormatter(
+                    mask: '+7 (###) ###-##-##', filter: {"#": RegExp(r'[0-9]')})
+              ],
+              decoration: const InputDecoration(
+                labelText: 'Номер телефона',
+                hintText: '+7 (___) ___-__-__',
               ),
             ),
-            
             paddingDivided,
-      
-            const TextField(
-              
-              decoration: InputDecoration(
-                hintText: 'Почта',
-                border: InputBorder.none,
-                floatingLabelBehavior: FloatingLabelBehavior.always,
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              inputFormatters: [
+                MaskTextInputFormatter(
+                    filter: {"#": RegExp(r'[a-zA-Z0-9@.]')})
+              ],
+              decoration: const InputDecoration(
+                labelText: 'Почта',
+                hintText: 'example@mail.com',
               ),
             ),
-
             paddingDivided,
-      
-            Text('Эти данные никому не передаются. После оплаты мы вышли чек на указанный вами номер и почту', style: theme.textTheme.titleMedium?.copyWith(color: grey),)
+            Text(
+              'Эти данные никому не передаются. После оплаты мы вышли чек на указанный вами номер и почту',
+              style: theme.textTheme.titleMedium?.copyWith(color: grey),
+            )
           ],
         ),
       ),
